9d54a04fae2f3f397ab1ba35694a07a2ff856604
akoustikakias <paok4geogeo@gmail.com>
2023-11-02 01:26:51 +0200

    writing to file

--- endec.c
+++ endec.c
@@ -9,7 +9,7 @@ FILE* encryptDataDH(char * message, mpz_t key[]){
 void encryptDataRSA(char * message, mpz_t key[]){
 	FILE * fp;
     FILE *output;
-    char *fName = "cipheredRSA.txt";
+    char *fName = "cipheredRSA2.txt";
 	int len = 0;
 	char c;
 	fp = fopen(message, "r");
@@ -31,27 +31,31 @@ void encryptDataRSA(char * message, mpz_t key[]){
     mpz_t ciphChar, msgChar;
     mpz_inits(msgChar, ciphChar, NULL);
     int i=0;
-    while ((c = fgetc(fp)) != EOF)
+    while (i<len)
     {
+        c = fgetc(fp);
         messageStr[i] = (char) c; //reading char one by one 
-        mpz_set_si(msgChar, (int)c);
+        mpz_set_ui(msgChar, (unsigned int)c);
         mpz_powm(ciphChar, msgChar, key[1], key[0]); // m = c^d mod n
         gmp_printf("%Zd\n", ciphChar);
-        cipherStr[i++]=(char)mpz_get_si(ciphChar);
-        fprintf(output,"%d\n",(int)mpz_get_si(ciphChar));
+        cipherStr[i++]=(char)mpz_get_ui(ciphChar);
+        //gmp_fprintf(output, "%Zd\n", ciphChar);
+        fprintf(output, "%u ", (unsigned)mpz_get_ui(ciphChar));
 
     }
     messageStr[i]='\0';
     cipherStr[i]='\0';
-    
    
     printf("[DEBUG]message is: %s\n", messageStr);
+
+    
 
     //preventing memory leaks
     mpz_clears(msgChar, ciphChar, NULL);
     free(messageStr);
-    fclose(fp);
     fclose(output);
+    fclose(fp);
+
 } 
 
 FILE* decryptDataDH(char* message, mpz_t key[]){
@@ -63,10 +67,11 @@ void decryptDataRSA(char* message, mpz_t key[]){
 	FILE * fp;
     FILE *output;
 	int len = 0;
-    char *fName = "cipheredRSA.txt";
+    char *fName = "cipheredRSA2.txt";
+    char *outputName = "decipheredRSA.txt";
 
-	fp = fopen(message, "r");
-    output = fopen(fName, "w");
+	fp = fopen(fName, "r");
+    output = fopen(outputName, "w");
 	//in case something goes wrong with files
 	if (fp==NULL || output==NULL){
 		printf("File error!!!\n");
@@ -81,14 +86,20 @@ void decryptDataRSA(char* message, mpz_t key[]){
     char ciphInt;
     mpz_inits(ciphChar, msgChar, NULL);
 
-    while (fscanf(fp, "%c", &ciphInt)!=EOF)
+    for (int i=0; i<len; i++)
     {
-        mpz_set_si(ciphChar, ciphInt);
+        ciphInt = fgetc(fp);
+        //fscanf(fp, "%c ", &ciphInt);
+        //gmp_fscanf(fp, "%Zd\n", &ciphChar);
+        gmp_printf("%Zd\n", ciphChar);
+        //mpz_set_si(ciphChar, ciphInt);
+        mpz_set_ui(ciphChar, (unsigned int)ciphInt);
         mpz_powm(msgChar, ciphChar, key[1], key[0]); // m = c^d mod n
-        printf("Character read: %c \n", ciphInt);
-        fprintf(output, "%u", (int)mpz_get_si(msgChar));
+        fprintf(stdout, "%c", (char)mpz_get_ui(msgChar));
+        i++;
     }
     printf("\n");
+
     mpz_clears(ciphChar, msgChar, NULL);
     fclose(fp);
     fclose(output);
