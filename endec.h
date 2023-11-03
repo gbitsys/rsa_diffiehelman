56ed50443011e69b6c885636234843f92786abaa
thanos karakos <103602897+thanosKarakos17@users.noreply.github.com>
2023-11-03 09:15:41 +0200

    Update endec.h

--- endec.h
+++ endec.h
@@ -2,8 +2,5 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-FILE* encryptDataDH(char *message, mpz_t key[]); //reads message from file encrypt and generates cipher text
-FILE* decryptDataDH(char *message, mpz_t key[]); //vise versa
-
-void encryptDataRSA(char *message, mpz_t key[]);
-void decryptDataRSA(char *message, mpz_t key[]);
+void encryptDataRSA(char * input_file, char* output_file, mpz_t key[]);
+void decryptDataRSA(char * input_file, char* output_file, mpz_t key[]);
