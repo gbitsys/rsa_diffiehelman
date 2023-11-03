6cbf0d4c0e781d9c2ad1576604ae211f864cb7d1
thanos karakos <103602897+thanosKarakos17@users.noreply.github.com>
2023-11-03 09:16:42 +0200

    Update keygen.h

--- keygen.h
+++ keygen.h
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<gmp.h>
-void keygen(mpz_t p,mpz_t q, mpz_t keyLength, mpz_t result[]); //generating RSA key-pair 
-int checkOdd(mpz_t p); //checks if a mpz_t number is odd
-int readNum();
-void epsilonGen(mpz_t lamda, mpz_t length,mpz_t result); //generating RSA encryption key
-void primeGen(mpz_t start, mpz_t end, mpz_t result);
+
+void keygen(mpz_t p,mpz_t q, mpz_t result[]); //generating RSA key-pair 
+void epsilonGen(mpz_t lamda, mpz_t result[]); //generating RSA encryption key
+void randomPair(mpz_t bitLength, mpz_t result[]);
